3b117c6693ef51d50456b81c34262a538c346646
bakarih <vocaljazz32@gmail.com>
2016-06-01 16:08:37 -0700

    building out remaining back end controllers and factories

--- server/controllers/topics.js
+++ server/controllers/topics.js
@@ -1,3 +1,6 @@
 // topics controller (Server side)
 // routes get processed here, use topic model (instance of Schema) to query database
 // http response from DB is sent as pure JSON to appropriate front end factory
+var mongoose = require('mongoose');
+var Topic = mongoose.model('Topic');
+var User = mongoose.model('User');
