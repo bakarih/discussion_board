c1092e23f891b36dd83f8ea6c27137483d985b01
bakarih <vocaljazz32@gmail.com>
2016-06-03 09:29:03 -0700

    finished post data flow

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,48 @@
 // posts controller (Server side)
 // routes get processed here, use post model (instance of Schema) to query database
 // http response from DB is sent as pure JSON to appropriate front end factory
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var User = mongoose.model('User');
+var Topic = mongoose.model('Topic');
+
+model.exports = (function()
+{
+	return {
+		addPost: function(req, res)
+		{
+			var post = new Post(req.body);
+			post.save(function(err, record){
+				if(err)
+				{
+					res.json({status: 'failed', err:err});
+				}
+				else
+				{
+					User.update({username: req.body.author}, {$inc: {posts: 1}}, {multi: true}, function(err1, record1)
+					{
+						if(err)
+						{
+							res.json({status: 'failed', err: err1});
+						}
+						else
+						{
+							Topic.update({_id: record.topic_id}, {$inc: {posts: 1}}, {multi: true}, function(err2, record2)
+							{
+								if(err)
+								{
+									res.json({status: 'failed', err: err2});
+								}
+								else
+								{
+									res.json({status: 'success'});
+								}
+							});
+						}
+					});
+				}
+			});
+		}
+	}
+
+});
