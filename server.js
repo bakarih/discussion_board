7695d27c7740d1bb9d405dc439df220b195b5b5a
bakarih <vocaljazz32@gmail.com>
2016-06-04 17:31:20 -0700

    changed port number

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ require('./server/config/mongoose.js');
 // this line requires and runs the code from our routes.js file and passes it app so that we can attach our routing rules to our express application!
 require('./server/config/routes.js')(app);
 
-app.listen(8080, function() {
-  console.log('cool stuff on: 8080');
+app.listen(5000, function() {
+  console.log('cool stuff on: 5000');
 });
